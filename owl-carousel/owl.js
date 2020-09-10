17758c54062b2310178d3a6fac39a1f7a4f1c337
sakibhosan416 <67134448+sakibhosan416@users.noreply.github.com>
2020-09-10 15:29:49 +0600

    mysixpak

---
+++ owl-carousel/owl.js
@@ -0,0 +1,55 @@
+$(document).ready(function() {
+ 
+      $("#owl-demo").owlCarousel({
+     
+          autoPlay: 3000, //Set AutoPlay to 3 seconds
+     
+          items : 4,
+          itemsDesktop : [1199,3],
+          itemsDesktopSmall : [979,3]
+     
+      });
+	       
+    });
+
+
+$(document).ready(function() {
+ 
+      $("#owl-demo2").owlCarousell({
+     
+          autoPlay: 4000, //Set AutoPlay to 3 seconds
+     
+          items : 4,
+          itemsDesktop : [1199,3],
+          itemsDesktopSmall : [979,3]
+     
+      });
+	       
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
